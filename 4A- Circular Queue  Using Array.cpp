4097cd35a709815a906026e34316cc3de3caf58c
saranya29022008-oss <saranya29022008@gmail.com>
2026-08-17 14:38:57 +0530

    Implement Circular Queue using Array in C++

---
+++ 4A- Circular Queue  Using Array.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+#define SIZE 5
+class CircularQueue {
+    int queue[SIZE];
+    int front, rear;
+public:
+    CircularQueue() {
+        front = -1;
+        rear = -1;
+    }
+
+    void enqueue(int value) {
+        if ((rear + 1) % SIZE == front) {
+            cout << "Queue is Full\n";
+            return;
+        }
+        if (front == -1)
+            front = 0;
+        rear = (rear + 1) % SIZE;
+        queue[rear] = value;
+        cout << value << " inserted\n";
+    }
+    void dequeue() {
+        if (front == -1) {
+            cout << "Queue is Empty\n";
+            return;
+        }
+        cout << queue[front] << " deleted\n";
+        if (front == rear) {
+            front = -1;
+            rear = -1;
+        } else {
+            front = (front + 1) % SIZE;
+        }
+    }
+    void display() {
+        if (front == -1) {
+            cout << "Queue is Empty\n";
+            return;
+        }
+        cout << "Circular Queue: ";
+        int i = front;
+        while (true) {
+            cout << queue[i] << " ";
+            if (i == rear)
+                break;
+            i = (i + 1) % SIZE;
+        }
+        cout << endl;
+    }
+};
+int main() {
+    CircularQueue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50);
+    q.display();
+    q.dequeue();
+    q.dequeue();
+    q.display();
+    q.enqueue(60);
+    q.enqueue(70);
+
+    q.display();
+    return 0;
+}
